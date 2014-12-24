3fcafbf219fbb8e3d17d798adb899f1ecd38b722
caspinall <colinaspinall@gmail.com>
2014-12-23 20:03:01 -0800

    First commit
    A project to play with

---
+++ InfiniteWorld/Assets/Resources/Scripts/TopDownCameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownCameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float followSpeed = 3;
+
+	void Start () 
+    {	
+	}	
+	
+	void Update () {
+        var newPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * followSpeed);
+	}
+}
---
+++ InfiniteWorld/Assets/Resources/Scripts/TopDownMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+
+    public Animator animator;    
+    public int speed = 5;
+    public int faceRightOffsetAngle;
+   
+    void Awake()
+    {
+    }	
+	
+	void Update () 
+    {
+        var movementVector = GetMovementVector();
+        MovementAnimation(movementVector);
+        FaceDirection(movementVector);
+        transform.position += movementVector;
+	}
+
+    private Vector3 GetMovementVector()
+    {
+        Vector3 newDirection = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        newDirection.Normalize();        
+        return newDirection * Time.deltaTime * speed;
+    }
+
+    private void MovementAnimation(Vector3 movementVector)
+    {
+        bool isMoving = movementVector != Vector3.zero;
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    private void FaceDirection(Vector3 movementVector)
+    {
+        if (movementVector != Vector3.zero)
+        {            
+            float angle = Mathf.Atan2(movementVector.y, movementVector.x) * Mathf.Rad2Deg + faceRightOffsetAngle;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}
